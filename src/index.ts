965e9a504a5ddb417c60f810da637a14373d0caf
Adel Obaji <adelobaji23@gmail.com>
2026-03-02 11:31:47 -0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 export const slug = (url: string): string => {
-  let newurl = url.toLowerCase().trim();
-  newurl = url.replace(/[" "_-]+/g, "-");
-  newurl = url.replace(/[!@#$%^&/.*-+]+/g, "");
-  newurl = newurl.charAt(0) === "-" ? (newurl = newurl.substring(1)) : newurl;
-  newurl =
-    newurl.charAt(newurl.length - 1) === "-"
-      ? (newurl = newurl.substring(0, newurl.length - 1))
-      : newurl;
-  return newurl;
+  let newurl = url
+    .toLowerCase()
+    .trim()
+    .replace(/[" "_-]+/g, "-")
+    .replace(/[!@#$%^&/.*+]+/g, "");
+  return cleanSlug(newurl);
+};
+const cleanSlug = (helpUrl: string): string => {
+  helpUrl =
+    helpUrl.charAt(0) === "-" ? (helpUrl = helpUrl.substring(1)) : helpUrl;
+  helpUrl =
+    helpUrl.charAt(helpUrl.length - 1) === "-"
+      ? (helpUrl = helpUrl.substring(0, helpUrl.length - 1))
+      : helpUrl;
+  return helpUrl;
 };
