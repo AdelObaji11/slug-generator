3f16c3aacf654cba2f63f8fc7938d32bc1bb3e4c
Adel Obaji <adelobaji23@gmail.com>
2026-03-02 08:49:42 -0900

    Update The Code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export function slug(s: string) {
-  return "slug";
-}
+export const slug = (url: string): string => {
+  let newurl = url.toLowerCase().trim();
+  newurl = url.replace(/[" "_-]+/g, "-");
+  newurl = url.replace(/[!@#$%^&/.*-+]+/g, "");
+  newurl = newurl.charAt(0) === "-" ? (newurl = newurl.substring(1)) : newurl;
+  newurl =
+    newurl.charAt(newurl.length - 1) === "-"
+      ? (newurl = newurl.substring(0, newurl.length - 1))
+      : newurl;
+  return newurl;
+};
