39deb0f2859dd0cc6b047d0e953b758b38bfdb80
Adel Obaji <adelobaji23@gmail.com>
2026-03-15 13:01:37 -0900

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export const slug = (url: string): string => {
   return trimLeadingAndTrillingDashes(newurl);
 };
 const trimLeadingAndTrillingDashes = (url: string): string => {
-  url = url.startsWith("-") ? (url = url.substring(1)) : url;
-  url = url.endsWith("-") ? (url = url.substring(0, url.length - 1)) : url;
+  url = url.startsWith("-") ? url.substring(1) : url;
+  url = url.endsWith("-") ? url.substring(0, url.length - 1) : url;
   return url;
 };
